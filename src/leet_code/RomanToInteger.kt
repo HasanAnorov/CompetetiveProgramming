718eaa00ae077e12b8acd82d03bae0585f054737
HasanAnorov <anorovhasan@gmail.com>
2024-09-18 23:57:37 +0500

    Problem no 9 solved - Palindrome Number.kt

---
+++ src/leet_code/RomanToInteger.kt
@@ -0,0 +1,15 @@
+package leet_code
+
+import java.util.Scanner
+
+fun main(){
+    val reader = Scanner(System.`in`)
+
+    println("Enter roman number - ")
+    val romanNumber = reader.nextLine()
+
+}
+
+fun romanToInt(s:String):Int{
+    
+}
