b805d12bda8ad07a3928d2e7be373ee555af1982
HasanAnorov <anorovhasan@gmail.com>
2025-01-05 11:25:31 +0500

    Longest Common Prefix, problem - 14 solved

--- src/leet_code/LongestCommonPrefix.kt
+++ src/leet_code/LongestCommonPrefix.kt
@@ -3,13 +3,21 @@ package leet_code
 class LongestCommonPrefix {
 
     fun longestCommonPrefix(strs: Array<String>): String {
-        val commonPrefix = ""
-        return commonPrefix
+        if (strs.isEmpty()) return ""
+        var prefix = strs[0]
+        for (i in 1..<strs.size) {
+            while (strs[i].indexOf(prefix) != 0) {
+                prefix = prefix.substring(0, prefix.length - 1)
+                if (prefix.isEmpty()) return ""
+            }
+        }
+        return prefix
     }
 
 }
 
 
 fun main() {
-
+    val test = LongestCommonPrefix()
+    test.longestCommonPrefix(arrayOf("sadas", "asdsadsa", "sadsfasdfsdf"))
 }
